c6c449f7c5d28ddd1e2b00bc6269cc1bf5c96589
agonzalez2022198 <agonzalez-2022198@kinal.edu.gt>
2023-09-07 14:01:36 -0600

    Proyecto actualizado

--- KiCine2/src/java/controlador/Controlador.java
+++ KiCine2/src/java/controlador/Controlador.java
@@ -11,12 +11,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import modelo.Usuario;
+import modeloDAO.UsuarioDAO;
 
 /**
  *
  * @author Windows 10
  */
 public class Controlador extends HttpServlet {
+
+    Usuario usuario = new Usuario();
+    UsuarioDAO usuarioDAO = new UsuarioDAO();
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -31,10 +36,54 @@ public class Controlador extends HttpServlet {
             throws ServletException, IOException {
         String menu = request.getParameter("menu");
         String accion = request.getParameter("accion");
-        if (menu.equals("Principal")){
+        if (menu.equals("Principal")) {
             request.getRequestDispatcher("Principal.jsp").forward(request, response);
+
+        } else if (menu.equals("Cartelera")) {
+            request.getRequestDispatcher("Cartelera.jsp").forward(request, response);
+
+        } else if (menu.equals("Cine")) {
+            request.getRequestDispatcher("Cine.jsp").forward(request, response);
+
+        } else if (menu.equals("Comentario")) {
+            request.getRequestDispatcher("Comentario.jsp").forward(request, response);
+
+        } else if (menu.equals("Estreno")) {
+            request.getRequestDispatcher("Estreno.jsp").forward(request, response);
+
+        } else if (menu.equals("Factura")) {
+            request.getRequestDispatcher("Factura.jsp").forward(request, response);
+
+        } else if (menu.equals("FuncionEspecial")) {
+            request.getRequestDispatcher("FuncionEspecial.jsp").forward(request, response);
+
+        } else if (menu.equals("HorariosProyeccion")) {
+            request.getRequestDispatcher("HorariosProyeccion.jsp").forward(request, response);
+
+        } else if (menu.equals("Pelicula")) {
+            request.getRequestDispatcher("Pelicula.jsp").forward(request, response);
+
+        } else if (menu.equals("usuario")) {
+
+            if (accion.equals("agregar")) {
+
+                String name = request.getParameter("nombre");
+                String apell = request.getParameter("apellido");
+                String email = request.getParameter("correo");
+                String contra = request.getParameter("contrasena");
+
+                usuario.setNombre(name);
+                usuario.setApellido(apell);
+                usuario.setCorreo_electronico(email);
+                usuario.setContrasena(contra);
+                usuarioDAO.agregar(usuario);
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+            } else if (accion.equals("cuenta")) {
+                request.getRequestDispatcher("AgregarUsuario.jsp").forward(request, response);
+            }
+            
         }
-        }
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
